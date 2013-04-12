a06576cf2d99e46966add9d5ee16be8056fd1fd6
marvinO1 <marvin01@bluewin.ch>
2013-04-12 09:22:58 +0200

    cpp

---
+++ cpp/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,154 @@
+// ConsoleApplication1.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#pragma comment(lib, "LogitechLcd.lib")
+
+#include "stdafx.h"
+#include "LogitechLcd.h"
+
+#define LOGITEC_DISPLAY_FOLDER_ROOT "LOGITEC_DISPLAY_FOLDER_ROOT"
+#define LOGITEC_DISPLAY_FOLDER_ROOT_DEFAULT "C:/temp/logitec"
+
+using namespace std;
+
+char* getRootPath() {
+	char* pPath;
+	size_t len;
+	errno_t err = _dupenv_s(&pPath, &len, LOGITEC_DISPLAY_FOLDER_ROOT );
+	if (pPath == NULL) {
+		pPath = *&LOGITEC_DISPLAY_FOLDER_ROOT_DEFAULT;
+		printf ("Environemnt: %s not set, using default root directory: %s\n", LOGITEC_DISPLAY_FOLDER_ROOT, pPath);
+	}
+	return pPath;
+}
+
+void removeFile(_finddata_t c_file) {
+	printf ("Removing file: %s\n", c_file.name);
+	remove(c_file.name);
+
+}
+
+
+void sendContentToLogitec(char* line0, char* line1, char* line2, char* line3) {
+	printf("Zeile[0]: %s \n", line0);
+	printf("Zeile[1]: %s \n", line1);
+	printf("Zeile[2]: %s \n", line2);
+	printf("Zeile[3]: %s \n", line3);
+
+	wchar_t  ws[100];
+	swprintf(ws, 100, L"%hs", line0);
+	LogiLcdMonoSetText(0, ws);
+
+    swprintf(ws, 100, L"%hs", line1);
+	LogiLcdMonoSetText(1, ws);
+		
+	swprintf(ws, 100, L"%hs", line2);
+	LogiLcdMonoSetText(2, ws);
+	
+	swprintf(ws, 100, L"%hs", line3);
+	LogiLcdMonoSetText(3, ws);
+
+	LogiLcdUpdate();
+}
+
+
+void displayFileContent(_finddata_t c_file) {
+    int idx = 0;
+	fstream f;
+	char lArray[4][1024];
+
+	// initialize the array so we have at least in each string a "".
+	for (int i = 0; i < 4; i++) {
+		 strcpy_s(lArray[i], "");
+	}
+
+	printf ("Going to read file: %s\n", c_file.name);
+    f.open(c_file.name, ios::in);
+    while (!f.eof() && idx <= 3 ) {
+    	f.getline(lArray[idx], 1024);    	
+    	idx++;
+    }
+    f.close();
+
+	sendContentToLogitec(lArray[0], lArray[1], lArray[2], lArray[3]);
+}
+
+long handleFiles() {
+
+	long hFile;
+	struct _finddata_t c_file;
+
+	 // Would be nice to read the oldest file first, so we would have some kind of FIFO queue!
+	if ((hFile = _findfirst("*.message", &c_file)) == -1L) {
+		printf ("No Files found!\n");
+		
+	} else {		
+		printf ("Found file: %s\n", c_file.name);
+		// wait a short time to be sure the file has been written
+		Sleep(100);
+		displayFileContent(c_file);
+		removeFile(c_file);		
+	}
+	_findclose(hFile);
+
+	// we could read the dispaly time from the file name, by now we just display a message 
+	// at least 5 seconds. This is ok for the first approach!
+	return 5;
+}
+
+
+void waitBeforeDisplayNextFile(int seconds) {
+  Sleep(seconds *  1000);
+}
+
+void checkButtonsBeforeDisplayNextFile(int seconds) {
+
+	boolean checkButtons = true;
+
+	int i = 0;
+	while (checkButtons) {
+	  LogiLcdUpdate();
+	  if (LogiLcdIsButtonPressed(LOGI_LCD_MONO_BUTTON_0)) {
+		 printf("Button 0 pressed ...\n");
+	  }
+	  if (LogiLcdIsButtonPressed(LOGI_LCD_MONO_BUTTON_1)) {
+		printf("Button 1 pressed ...\n");
+	  }
+	  if (LogiLcdIsButtonPressed(LOGI_LCD_MONO_BUTTON_2)) {
+		printf("Button 2 pressed ...\n");
+	  }
+	  if (LogiLcdIsButtonPressed(LOGI_LCD_MONO_BUTTON_3)) {
+		printf("Button 3 pressed ...\n");
+	  }
+
+	  // if not reached the max time to wait, we stay in the loop.
+	  if (i * 100 < seconds * 1000) {
+		  Sleep(100);
+		  i++;
+	  } else {
+		  printf("leaving loop to chec buttons!");
+		  checkButtons = false;
+	  }
+	}
+}
+
+int _tmain(int argc, _TCHAR* argv[]) {
+
+	wchar_t * text = L"NCA-LogitecHub";
+	LogiLcdInit(text, LOGI_LCD_TYPE_MONO);
+
+	printf("Running ConsoleApplication1 ...\n");
+	char* pPath = getRootPath();
+	if (pPath != NULL) {
+	  printf ("Current path to look up files is: %s\n", pPath);
+
+	   _chdir(pPath);
+	   while(true) {
+		   long displayTime = handleFiles();
+		   checkButtonsBeforeDisplayNextFile(displayTime);
+	   }
+	} else {
+	  printf ("%s is not set, aborting programm!", LOGITEC_DISPLAY_FOLDER_ROOT);
+	}
+	return 0;
+}
---
+++ cpp/ConsoleApplication1/ConsoleApplication1/ReadMe.txt
@@ -0,0 +1,26 @@
+﻿ConsoleApplication1
+===================
+
+Environment Settings
+--------------------
+ LOGITEC_DISPLAY_FOLDER_ROOT, point to the directory the application reads and writes files. The directory contains 
+ following sub folders:
+
+    inbound, directory where inbound messages can be placed which will then be displayed on the logitc gamepad.
+	the file must have follwoing naming pattern <id>.<nn>.message.
+	   
+	   id: unique id of the producer of the file
+	   nn: time in seconds the message should be displayed at least on the pad
+	         
+
+    outbound, can contain up to 4 files: b0.pressed, b1.pressed, b2.pressed and b3.pressed. The actual time stamp
+	of the files indicate when the buttons on the pad have been pressed. If a file does not exist, the button was
+	never pressed.
+
+
+    Example:
+	  LOGITEC_DISPLAY_FOLDER_ROOT=C:\Users\Beat\.logitec
+
+	   C:\Users\Beat\.logitec\inbound\jenkins.50.message
+	   C:\Users\Beat\.logitec\outbound\b1.pressed
+	   C:\Users\Beat\.logitec\outbound\b3.pressed
